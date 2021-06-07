d3a23ceac9ee3549c404208a6a68c87dda50fa27
Jafer Daltro <jaferjr@gmail.com>
2021-06-07 11:25:56 -0300

    add model games

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
+
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
   config.change_headers_on_each_request = true
-
+  
+  config.require_client_password_reset_token = true
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
   config.token_lifespan = 1.weeks
